d3978b3546d240b638442e136e664033d163834d
Simon-Fuhaoyuan <44106243+Simon-Fuhaoyuan@users.noreply.github.com>
2020-02-24 12:29:23 +0800

    Day 82

---
+++ invertBinaryTree/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (!root)
+            return root;
+        invertTree(root->left);
+        invertTree(root->right);
+        TreeNode* tmp = root->left;
+        root->left = root->right;
+        root->right = tmp;
+        return root;
+    }
+};
